d49406a35d390b729173be8610c05ffe4d9edafa
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2023-09-03 20:59:01 -0400

    g

--- api/main.py
+++ api/main.py
@@ -2,7 +2,6 @@
 from fastapi import FastAPI 
 
 from api.openapi.handler import router as openapi_router
-
 app = FastAPI()
 
 app.include_router(openapi_router, prefix="/openapi")
--- api/openapi/handler.py
+++ api/openapi/handler.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+import ee
 
 router = APIRouter()
-
 @router.get("/v1/chat/completions")
 async def root():
     print("hello world")
+    ee.test()
     return {"message": "Hello World"}
