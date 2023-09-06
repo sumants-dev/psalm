9c628effcfbb391852fc300721508a26d5758a36
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2023-09-06 19:24:33 -0400

    ..

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI 
 
-from api.openai.handler import router as openapi_router
+from api.llm.handler import router as openapi_router
 app = FastAPI()
 
-app.include_router(openapi_router, prefix="/openapi")
+app.include_router(openapi_router, prefix="/llm")
--- pkgs/config/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import PostgresDsn
-
-    
-class Settings(BaseSettings):
-    app_name: str = "Pontus PSaLM"
-    postgres_db_uri: PostgresDsn | None  = None
-    model_config = SettingsConfigDict(env_file=".env")
