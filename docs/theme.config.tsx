ded60728923b9681a4f4b9cbdaf80bf84d81f3ac
Sumant R Shringari <61118035+sumants-dev@users.noreply.github.com>
2023-09-15 13:30:57 -0400

    Update docs (#16)

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -3,6 +3,8 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span> Pontus </span>,
+  docsRepositoryBase: "https://github.com/PontusAI/psalm",
+  editLink: { "text": "Edit this page on GitHub", component: null},
 }
 
 export default config
